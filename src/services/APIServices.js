8c0fe95f19e788f36d7f4b06b5c2e2bf8d96f2e1
Max <tutv95@gmail.com>
2018-04-14 21:12:44 +0700

    Added Storage Services

--- src/services/APIServices.js
+++ src/services/APIServices.js
@@ -1,3 +1,47 @@
-export const makeRequest = () => {
+import axios from "axios";
+import {getLocalData} from "./StorageServices";
+import getEnv from "../helpers/getEnv";
 
+const _getAppVersion = () => {
+    const app = getLocalData('app');
+
+    return app ? app.version : '1.0.0';
 };
+
+class APIServices {
+    constructor(baseURL) {
+        const _baseURL = baseURL || getEnv('baseAPIUrl');
+
+        this.api = axios.create({
+            baseURL: _baseURL,
+            timeout: 15000,
+        });
+    }
+
+    makeRequest(args) {
+        const _headers = args.headers ? args.headers : {};
+
+        const defaultHeaders = {
+            'X-App-Version': _getAppVersion(),
+        };
+
+        args = {
+            ...args,
+            headers: {
+                ...defaultHeaders,
+                ..._headers
+            }
+        };
+
+        return this.api(args)
+            .then(({data}) => {
+                return Promise.resolve(data);
+            });
+    }
+}
+
+export const APIServicesExtend = (baseURL) => {
+    return new APIServices(baseURL);
+};
+
+export default new APIServices();
