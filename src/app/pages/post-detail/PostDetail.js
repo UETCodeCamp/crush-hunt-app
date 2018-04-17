969ca72db3c573f11a20ee5596cde5b3cf667394
Flashxu <dinhxuan9807@gmail.com>
2018-04-17 14:26:03 +0700

    loi jh the

--- src/app/pages/post-detail/PostDetail.js
+++ src/app/pages/post-detail/PostDetail.js
@@ -6,9 +6,7 @@ class PostDetail extends Component {
 
         return (
             <div className="PostDetail">
-                <h1>Post Detail</h1>
-                <h2>1123581321</h2>
-                <h3>lan 3</h3>
+                
             </div>
         );
     }
