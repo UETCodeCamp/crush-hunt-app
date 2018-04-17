2b7504f9837db7c815511afff5b1e3f8fda8639f
PPbra <16021629@vnu.edu.vn>
2018-04-17 23:26:43 +0700

    demo homepages.js

--- src/app/pages/home/Comment.js
+++ src/app/pages/home/Comment.js
@@ -1,26 +1,33 @@
-import React , {Component} from 'react';
+import React , {Component} from "react";
+
 
 class Comment extends React.Component{
 
     render(){
 
+        const listComent = this.props.dataPost.listComments.map((data)=>{
+            return <div className={'comment'} ><span className={'userComment'}>{data.userNameComment}</span>{data.commentText}</div>
+        });
+
         return (
             <div>
-
-                <div>
-                    <span className={'like'}/>
-                    <span className={'commnetExpand'}/>
-                    <div className={'likesNumber'}>
-                        <a  href=""/>
-                    </div>
+                <div className="likeButton">
+                    <span className="unlike"/>
+                    <span className="comment"/>
                 </div>
-
-                <div className={'listComment'}>
-                    <ul />
+                <div className="likeText">
+                    {this.props.dataPost.likeText}
                 </div>
-
+                <div className="postText">
+                    {this.props.dataPost.postText}
+                </div>
+                <div className={'commentList'}>
+                    {listComent}
+                </div>
+                <div className="time">
+                </div>
             </div>
-        )
+        );
     }
 }
 
