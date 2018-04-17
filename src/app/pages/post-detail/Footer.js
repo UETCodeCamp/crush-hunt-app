a4f08e430134f15e0a837c5d4f7a6eb7634714ef
tranhieu956230 <tranhieu956230@gmail.com>
2018-04-17 16:06:14 +0700

    local commit

--- src/app/pages/post-detail/Footer.js
+++ src/app/pages/post-detail/Footer.js
@@ -4,7 +4,24 @@ class Footer extends React.Component{
     render(){
         return(
             <footer id="post-detail-footer">
-                <h1>Footer</h1>
+                 <nav >
+                 <ul>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">SUPPORT</a></li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">PRESS</a></li>
+                    <li><a href="#">API</a></li>
+                    <li><a href="#">JOBS</a></li>
+                    <li><a href="#">PRIVACY</a></li>
+                    <li><a href="#">TERMS</a></li>
+                    <li><a href="#">DIRECTORY</a></li>
+                    <li><a href="#">PROFILES</a></li>
+                    <li><a href="#">HASHTAGS</a></li>
+                    <li><a href="#">LANGUAGE</a></li>
+                    
+                 </ul>
+                 <span>&copy; 2018 UETCODECAMP</span>
+                 </nav>
             </footer>
         )
     }
