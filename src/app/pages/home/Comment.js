4fcad5ae446eb0af1df48dc410906017d9119b70
hpbdmylove <iammaytinhdibo@gmail.com>
2018-04-18 22:45:11 +0700

    fix top comment

--- src/app/pages/home/Comment.js
+++ src/app/pages/home/Comment.js
@@ -1,31 +1,28 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 
 
-class Comment extends React.Component{
+class Comment extends React.Component {
 
     state = {
-        commentsShow:[],
-        isShowComment:false,
+        commentsShow: this.props.dataPost.topComments,
+        isShowComment: false,
     }
 
-    render(){
+    render() {
 
 
-        const listComent = this.state.commentsShow.map((data)=>{
+        const listComent = this.state.commentsShow.map((data) => {
             return <div className={'comment'} ><span className={'userComment'}>{data.userNameComment}</span>{data.commentText}</div>
         });
 
         return (
             <div>
                 <div className="likeButton">
-                    <span className="unlike"/>
-                    <span className="comment"/>
+                    <span className="unlike" />
+                    <span className="comment" />
                 </div>
                 <div className="likeText">
                     {this.props.dataPost.likeText}
-                </div>
-                <div className={'loadComment'} onClick={this.loadComment} id={this.props.dataPost.userName}>
-                    Load more comments...
                 </div>
                 <div className="postText">
                     {this.props.dataPost.postText}
@@ -33,21 +30,24 @@ class Comment extends React.Component{
                 <div className={'commentList'}>
                     {listComent}
                 </div>
+                <div className={'loadComment'} onClick={this.loadComment} id={this.props.dataPost.userName}>
+                    Load more comments...
+                </div>
             </div>
         );
     }
 
-    loadComment= (e)=>{
-        if(!this.state.isShowComment){
+    loadComment = (e) => {
+        if (!this.state.isShowComment) {
             this.setState({
-                commentsShow:this.props.dataPost.comments,
-                isShowComment:true,
+                commentsShow: this.props.dataPost.comments,
+                isShowComment: true,
             })
             document.getElementById(this.props.dataPost.userName).innerText = 'Hiden comment...';
-        }else{
+        } else {
             this.setState({
-                commentsShow:this.props.dataPost.topComments,
-                isShowComment:false,
+                commentsShow: this.props.dataPost.topComments,
+                isShowComment: false,
             })
             document.getElementById(this.props.dataPost.userName).innerText = 'Load more comments...';
         }
@@ -55,4 +55,4 @@ class Comment extends React.Component{
     }
 }
 
-export  default  Comment;
+export default Comment;
--- src/app/pages/home/HomePage.js
+++ src/app/pages/home/HomePage.js
@@ -13,33 +13,33 @@ class HomePage extends Component {
                 imgSrc: 'https://s.abcnews.com/images/Entertainment/HT_TSpelling_MEM_160101_1x1_1600.jpg',
                 likeText: ['không ai thèm like'],
                 timePost: 1523986145507,
-                postText: "ahihihi tui dang ne!",
-                topComments:[
-                        {
-                            userNameComment: 'ahihi1',
-                            commentText: 'hehehe'
-                        },
-                        {
-                            userNameComment: 'ahihi2',
-                            commentText: 'hahaha'
-                        }
-                    ]
+                postText: "Sinh nhật zui zẻ cùng với các pé iu của Dép!",
+                topComments: [
+                    {
+                        userNameComment: 'Bà Zôn Xơn',
+                        commentText: 'không like cho Dép nha.'
+                    },
+                    {
+                        userNameComment: 'Cô Zô Dép',
+                        commentText: 'chị Xơn quá đáng lắm lun á'
+                    }
+                ]
 
                 ,
-                comments:[
-                        {
-                            userNameComment: 'ahihi1',
-                            commentText: 'hehehe'
-                        },
-                        {
-                            userNameComment: 'ahihi2',
-                            commentText: 'hahaha'
-                        },
-                        {
-                            userNameComment:'noname',
-                            commentText:'hahah eheheh'
-                        }
-                    ],
+                comments: [
+                    {
+                        userNameComment: 'Bà Zôn Xơn',
+                        commentText: 'không like cho Dép nha.'
+                    },
+                    {
+                        userNameComment: 'Cô Zô Dép',
+                        commentText: 'chị Xơn quá đáng lắm lun á'
+                    },
+                    {
+                        userNameComment: 'noname',
+                        commentText: 'hahah eheheh'
+                    }
+                ],
             }, {
                 userName: 'Tui la ai',
                 imgSrc: 'https://scontent.fhan5-3.fna.fbcdn.net/v/t1.0-9/30706179_985237394984694_513570529735606272_n.jpg?_nc_cat=0&oh=1ad1c706fdda579d1cdbaf9782094e7e&oe=5B7494DB',
@@ -47,32 +47,32 @@ class HomePage extends Component {
                 timePost: 1523303045507,
                 postText: "Phuong dep trai",
 
-                topComments:[
-                            {
-                                userNameComment: 'ahihi1',
-                                commentText: 'dep qua ne'
-                            },
-                            {
-                                userNameComment: 'ahihi2',
-                                commentText: 'ok hehe dep!'
-                            }
-                        ],
-                comments:[
-                            {
-                                userNameComment: 'ahihi1',
-                                commentText: 'de qua ne'
-                            },
-                            {
-                                userNameComment: 'ahihi2',
-                                commentText: 'ok hehe dep'
-                            },
-                            {
-                                userNameComment:'noname',
-                                commentText:'hahah eheheh'
-                            }
-                        ],
+                topComments: [
+                    {
+                        userNameComment: 'ahihi1',
+                        commentText: 'dep qua ne'
+                    },
+                    {
+                        userNameComment: 'ahihi2',
+                        commentText: 'ok hehe dep!'
+                    }
+                ],
+                comments: [
+                    {
+                        userNameComment: 'ahihi1',
+                        commentText: 'de qua ne'
+                    },
+                    {
+                        userNameComment: 'ahihi2',
+                        commentText: 'ok hehe dep'
+                    },
+                    {
+                        userNameComment: 'noname',
+                        commentText: 'hahah eheheh'
+                    }
+                ],
 
-                }
+            }
             ],
         }
     }
