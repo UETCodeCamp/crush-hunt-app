3e35296b357ae34f30a91461dc553549574ab4c6
Hap Tien Quan <zed.quan99@gmail.com>
2018-04-19 23:29:28 +0700

    add style

--- src/app/pages/upload/UploadPage.js
+++ src/app/pages/upload/UploadPage.js
@@ -1,18 +1,42 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import "./Style.css";
 
 class UploadPage extends Component {
     render() {
         return (
             <div className="UploadPage">
-                <h1>Upload a post 123</h1>
+                <section className="Upload">
+                    <div className="content">
+                        <h1>UPLOAD IMAGE</h1>
+
+                        <form className="input">
+                            <div>
+                                <input className="link" placeholder="Link ảnh"/>
+
+                                <button>Brower</button>
+                            </div>
+
+                            <img className="thumb" src="https://3.bp.blogspot.com/-tw-rSrfdYq8/V44qWz9DezI/AAAAAAAAAtQ/a_h_avH6RKEwnfN9KNAjarW5bybcilnowCLcB/s640/hinh-anh-girl-xinh-de-thuong-12.jpg"/>
+
+                            <div className="comment">
+                                <input placeholder="Nói gì đó về bức ảnh"/>
+
+                                <input placeholder="Thêm địa điểm check in"/>
+                            </div>
+
+
+                            <div >
+                                <button>Đăng Ảnh</button>
+
+                                <button>Hủy Bỏ</button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
             </div>
         );
     }
 }
-
-UploadPage.propTypes = {
-    history: PropTypes.object
-};
 
 export default UploadPage;
