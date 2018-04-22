c297bd8233bf9856b2437ccf846df95e3ded5495
tranhieu956230 <tranhieu956230@gmail.com>
2018-04-22 16:49:04 +0700

    merge

--- src/app/pages/post-detail/PostDetail.js
+++ src/app/pages/post-detail/PostDetail.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./PostDetailPage.css";
 import DashBoardPostDetails from "./DashBoardPostDetails";
-
+import Footer from "../../shared-components/footer/Footer"
 class PostDetail extends Component {
     render() {
         return (
+            <div>
             <div id="post-detail-main">
                 <div className="wrap">
                     <DashBoardPostDetails />
                 </div>
+            </div>
+            <Footer/>
             </div>
         );
     }
