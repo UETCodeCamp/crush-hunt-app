773f2d3662382beec78de36d0bc19abc26ed87d7
Hap Tien Quan <zed.quan99@gmail.com>
2018-04-22 07:47:44 +0700

    test

--- src/app/pages/upload/UploadPage.js
+++ src/app/pages/upload/UploadPage.js
@@ -34,38 +34,9 @@ class UploadPage extends Component {
                                         <line  x1="8" x2="10" y1="14" y2="4"/>
                                     </svg>
                                 </button>
-                                <button className="ql-blockquote" type="button">
-                                    <svg viewBox="0 0 18 18">
-                                        <rect  height="3" width="3" x="4" y="5"/>
-                                        <rect  height="3" width="3" x="11" y="5"/>
-                                        <path  d="M7,8c0,4.031-3,5-3,5"/>
-                                        <path  d="M14,8c0,4.031-3,5-3,5"/>
-                                    </svg>
-                                </button>
-                                <button className="ql-list" value="bullet" type="button">
-                                    <svg viewBox="0 0 18 18">
-                                        <line  x1="6" x2="15" y1="4" y2="4"/>
-                                        <line  x1="6" x2="15" y1="9" y2="9"/>
-                                        <line  x1="6" x2="15" y1="14" y2="14"/>
-                                        <line  x1="3" x2="3" y1="4" y2="4"/>
-                                        <line  x1="3" x2="3" y1="9" y2="9"/>
-                                        <line  x1="3" x2="3" y1="14" y2="14"/>
-                                    </svg>
-                                </button>
-                                <button className="ql-list" value="ordered" type="button">
-                                    <svg viewBox="0 0 18 18">
-                                        <line className="ql-stroke" x1="7" x2="15" y1="4" y2="4"/>
-                                        <line className="ql-stroke" x1="7" x2="15" y1="9" y2="9"/>
-                                        <line className="ql-stroke" x1="7" x2="15" y1="14" y2="14"/>
-                                        <line className="ql-stroke ql-thin" x1="2.5" x2="4.5" y1="5.5" y2="5.5"/>
-                                        <path className="ql-fill"
-                                              d="M3.5,6A0.5,0.5,0,0,1,3,5.5V3.085l-0.276.138A0.5,0.5,0,0,1,2.053,3c-0.124-.247-0.023-0.324.224-0.447l1-.5A0.5,0.5,0,0,1,4,2.5v3A0.5,0.5,0,0,1,3.5,6Z"/>
-                                        <path className="ql-stroke ql-thin"
-                                              d="M4.5,10.5h-2c0-.234,1.85-1.076,1.85-2.234A0.959,0.959,0,0,0,2.5,8.156"/>
-                                        <path className="ql-stroke ql-thin"
-                                              d="M2.5,14.846a0.959,0.959,0,0,0,1.85-.109A0.7,0.7,0,0,0,3.75,14a0.688,0.688,0,0,0,.6-0.736,0.959,0.959,0,0,0-1.85-.109"/>
-                                    </svg>
-                                </button>
+
+
+
                                 <button className="ql-smiley" type="button">
                                     <svg viewBox="0 0 18 18">
                                         <circle className="ql-fill" cx="7" cy="7" r="1"/>
