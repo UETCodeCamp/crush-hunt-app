2659e364390a7df2b04cf883a96232e5f569221f
vansi99 <vansido99@gmail.com>
2018-04-21 18:23:58 -0700

    update

---
+++ src/app/pages/upload/ImageUpload.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import cameraImg from "./image/camera.png";
+import "./UploadPage.css"
+
+
+class ImageUpload extends Component {
+
+    render() {
+        return (
+            <div className="img_upload"><img className="thumb" alt="camera" src={cameraImg} />
+            </div>
+        );
+    }
+
+}
+
+
+export default ImageUpload;
